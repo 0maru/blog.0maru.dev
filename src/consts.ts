dc3fe892f4e009d8f94159a640bfc44ae77ea55b
Toi Yoshimaru <0maru.dev@gmail.com>
2023-10-24 14:25:04 +0900

    画像をR2にアップロードした

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = '0maru Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const IMAGE_DOMAIN = 'blog-images.0maru.dev'
--- src/pages/posts/create_blog copy.md
+++ src/pages/posts/create_blog copy.md
@@ -1,7 +1,7 @@
 ---
 layout: ../../layouts/PostLayout.astro
 title: AstroとCloudflareでブロクを作ってみた
-image: http://localhost:4321/images/fujikokyoto0H0264_TP_V.webp
+image: https://https://blog-images.0maru.dev/fujikokyoto0H0264_TP_V.webp
 createdAt: 2023-10-01
 tags: ["Astro", "Cloudflare", "環境"]
 ---
