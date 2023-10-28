0d98ecb0d6857b864a9af32d894dbc81b529ccc7
Toi Yoshimaru <0maru.dev@gmail.com>
2023-10-28 23:58:32 +0900

    astro.config に設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,11 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://blog.0maru.dev',
   integrations: [
     mdx(),
     sitemap(),
     partytown(),
   ],
+  outDir: './dist',
 });
