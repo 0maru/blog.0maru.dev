63a2c2da88430dde85c4efc8962888084b122e83
Toi Yoshimaru <0maru.dev@gmail.com>
2023-10-29 02:00:28 +0900

    description を追加

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'blog.0maru.dev';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '0maru のブログです。エンジニアリングとかガジェットやその他諸々について書き連ねていきます。';
 export const IMAGE_DOMAIN = 'blog-images.0maru.dev'
