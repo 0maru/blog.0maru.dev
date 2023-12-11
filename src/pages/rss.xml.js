1988be68089cdce747acf3e8bf3d8938f0391d43
Toi Yoshimaru <0maru.dev@gmail.com>
2023-12-11 17:32:31 +0900

    rss を有効化

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,17 @@
-// import rss from '@astrojs/rss';
-// import { getCollection } from 'astro:content';
-// import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from '@astrojs/rss';
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
-// export async function GET(context) {
-// 	const posts = await getCollection('blog');
-// 	return rss({
-// 		title: SITE_TITLE,
-// 		description: SITE_DESCRIPTION,
-// 		site: context.site,
-// 		items: posts.map((post) => ({
-// 			...post.data,
-// 			link: `/blog/${post.slug}/`,
-// 		})),
-// 	});
-// }
+export async function GET(context) {
+  const postImportResult = import.meta.glob('../pages/posts/*.md', {eager: true})
+  const posts = Object.values(postImportResult)
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+        link: post.url,
+        ...post.frontmatter,
+      }),
+    )
+  });
+}
