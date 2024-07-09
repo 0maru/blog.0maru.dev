ff5266ea19f2ea822c8d83c8c6e2050f06c265ba
Toi Yoshimaru <0maru.dev@gmail.com>
2024-07-09 09:08:58 +0900

    biome check write

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,16 @@
 import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import {SITE_DESCRIPTION, SITE_TITLE} from '../consts';
 
 export async function GET(context) {
-  const postImportResult = import.meta.glob('../pages/posts/*.md', {eager: true})
-  const posts = Object.values(postImportResult)
+  const postImportResult = import.meta.glob('../pages/posts/*.md', {eager: true});
+  const posts = Object.values(postImportResult);
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map((post) => ({
-        link: post.url,
-        ...post.frontmatter,
-      }),
-    )
+      link: post.url,
+      ...post.frontmatter,
+    })),
   });
 }
