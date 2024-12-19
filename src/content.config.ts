6d7cc43e32f5791ef3ab8370036804a124d298c5
0maru <43493921+0maru@users.noreply.github.com>
2024-12-19 15:28:11 +0900

    コンテンツ一覧をdefineCollection で出せるようにした

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import {defineCollection, z} from 'astro:content';
 import {glob} from 'astro/loaders';
 
 const blog = defineCollection({
-  loader: glob({pattern: '*.md', base: './src/contents/blog'}),
+  loader: glob({pattern: '**/*.md', base: './src/contents/blog'}),
   schema: z.object({
     slug: z.string(),
     title: z.string(),
