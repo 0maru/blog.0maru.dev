98b571ef0a0c3a1e700784af556bfda7f54ea2ef
0maru <43493921+0maru@users.noreply.github.com>
2024-12-22 02:38:19 +0900

    extends で背景色を定義

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,10 +2,11 @@
 export default {
   content: ['./src/**/*.astro', './src/**/**/*.astro'],
   theme: {
-    colors: {
-      blueBlack: '#0a0b12',
+    extend: {
+      colors: {
+        blueBlack: '#0a0b12',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
