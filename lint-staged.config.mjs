b35d234f552f0375c95da939b62279614626a053
0maru <43493921+0maru@users.noreply.github.com>
2025-03-23 20:03:59 +0900

    biome.jsonにdistをignoreリストに追加し、lint-staged.config.mjsでastroファイルをprettierの対象に追加

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 export default {
-  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc,astro}': [
-    'biome check --write --no-errors-on-unmatched',
+  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': [
+    'biome check --write --no-errors-on-unmatched .',
   ],
-  '*.{md,yml,yaml}': ['prettier --write'],
+  '*.{md,yml,yaml,astro}': ['prettier --write .'],
 };
