8089b327920b1e7efcdd770359d9afbeacd2cab4
0maru <0maru.dev@gmail.com>
2026-06-21 14:54:15 +0900

    Fix Biome check failures

--- src/types/remark-link-card.d.ts
+++ src/types/remark-link-card.d.ts
@@ -1,5 +1,6 @@
 declare module 'remark-link-card' {
   import type {Plugin} from 'unified';
+
   const remarkLinkCard: Plugin<[{cache?: boolean; shortenUrl?: boolean}?]>;
   export default remarkLinkCard;
 }
